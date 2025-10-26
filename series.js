77934f1a0c8bab3cf388b7b0eb684e7d7218ef00
giithub2 <giithub2@outlook.com>
2025-10-26 18:31:03 +0530

    Update series.js

--- series.js
+++ series.js
@@ -102,7 +102,7 @@ document.write(`<!DOCTYPE html>
         dates: {
           "2025-10-26": [
             { startHour: 10, startMinute: 30, endHour: 14, endMinute: 40, message: "World Cup - 2025", image: "https://i.ibb.co/p6gPdBZr/file-29359.jpg" },
-            { startHour: 14, startMinute: 50, endHour: 18, endMinute: 0, message: "World Cup - 2025", image: "https://img.cricketnmore.com/uploads/2025/10/india-women-vs-bangladesh-women-prediction-match-28-icc-womens-world-cup-2025-who-will-win-today-ind-w-vs-ban-w-match.jpg" }
+            { startHour: 14, startMinute: 50, endHour: 21, endMinute: 0, message: "World Cup - 2025", image: "https://img.cricketnmore.com/uploads/2025/10/india-women-vs-bangladesh-women-prediction-match-28-icc-womens-world-cup-2025-who-will-win-today-ind-w-vs-ban-w-match.jpg" }
           ],
           "2025-10-29": [
             { startHour: 14, startMinute: 30, endHour: 21, endMinute: 0, message: "1st Simi Final", image: "https://i.ibb.co/gMS38L13/IMG-20251026-182033-912.jpg" }
