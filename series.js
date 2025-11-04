de36caa59251e3c38573ff34c736f8d0e74a2749
giithub2 <giithub2@outlook.com>
2025-11-04 07:50:54 +0530

    Update series.js

--- series.js
+++ series.js
@@ -89,7 +89,7 @@ document.write(`<!DOCTYPE html>
    <!-----> <a href="https://me.webcric.com/frame2.htm"> <!------>
    
       <div class="cardLive5">
-        <img src="https://i.ibb.co/zhnWcXsf/file-29358.jpg" alt="Card Image" id="cardLive5Image">
+        <img src="https://images.wondershare.com/filmora/article-images/css-loading-text-animation-1.gif" alt="Card Image" id="cardLive5Image">
         <div class="customMessage5"></div>
         <div class="liveIndicator5">Live Now</div>
         <div class="countdownTimer5"></div>
@@ -102,7 +102,7 @@ document.write(`<!DOCTYPE html>
       {
         match: "Fancode",
         dates: {
-          "2025-11-04": [
+          "2025-10-04": [
             { startHour: 15, startMinute: 30, endHour: 22, endMinute: 40, message: "1st ODI", image: "https://i.ibb.co/gFQdLNCQ/IMG-20251104-072817-416.jpg" },
             { startHour: 14, startMinute: 50, endHour: 21, endMinute: 0, message: "World Cup - 2025", image: "https://img.cricketnmore.com/uploads/2025/10/india-women-vs-bangladesh-women-prediction-match-28-icc-womens-world-cup-2025-who-will-win-today-ind-w-vs-ban-w-match.jpg" }
           ],
@@ -112,8 +112,8 @@ document.write(`<!DOCTYPE html>
           "2025-11-08": [
             { startHour: 15, startMinute: 30, endHour: 22, endMinute: e0, message: "3rd ODI", image: "https://i.ibb.co/gFQdLNCQ/IMG-20251104-072817-416.jpg" }
           ],
-          "2025-11-02": [
-            { startHour: 14, startMinute: 30, endHour: 23, endMinute: 50, message: "World Cup - Final", image: "https://i.ibb.co/p6G4n6xd/IMG-20251031-051927-822.jpg" }
+          "2025-11-04": [
+            { startHour: 15, startMinute: 30, endHour: 22, endMinute: 50, message: "1st ODI", image: "https://i.ibb.co/gFQdLNCQ/IMG-20251104-072817-416.jpg" }
           ]
         }
       }
