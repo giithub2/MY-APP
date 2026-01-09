8f3127113742e18d72152a36c50f5a07a014f450
giithub2 <giithub2@outlook.com>
2026-01-09 19:34:21 +0530

    Update series.js

--- series.js
+++ series.js
@@ -86,7 +86,7 @@ document.write(`<!DOCTYPE html>
 <body>
   <div class="cardLive5-container">
     <!---<a href="https://ongoing-series.blogspot.com/?s=1"><!----->
-    <!-----><a href="https://tnt-live-sports.blogspot.com/?m=1"><!----->
+    <!-----><a href="https://crichd.xfireflix.workers.dev/player?id=starhindi"><!----->
       <div class="cardLive5">
         <img src="https://images.wondershare.com/filmora/article-images/css-loading-text-animation-1.gif" alt="Card Image" id="cardLive5Image">
         <div class="customMessage5"></div>
