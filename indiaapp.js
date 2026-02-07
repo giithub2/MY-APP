2bbda07db4f1978bff53052910473ba9264a81a6
giithub2 <giithub2@outlook.com>
2026-02-07 19:26:31 +0530

    Update indiaapp.js

--- indiaapp.js
+++ indiaapp.js
@@ -94,12 +94,12 @@ document.write(`<!DOCTYPE html>
       {
         match: "Fancode",
         dates: {
-          "2026-02-07": [
+          "2026-02-06": [
             { startHour: 10, startMinute: 30, endHour: 19, endMinute: 0, message: "World Cup - 2026", image: "https://indiantvinfo.com/media/2026/02/Ind-Vs-USA.jpg" },
             { startHour: 19, startMinute: 0, endHour: 23, endMinute: 0, message: "World Cup - 2026", image: "&s=10" }
           ],
-          "2026-02-08": [
-            { startHour: 10, startMinute: 0, endHour: 23, endMinute: 0, message: "World Cup - 2026", image: "https://i.ibb.co/W4bJKLm3/IMG-20260131-081400-651.jpg" }
+          "2026-02-07": [
+            { startHour: 10, startMinute: 0, endHour: 23, endMinute: 0, message: "World Cup - 2026", image: "https://indiantvinfo.com/media/2026/02/Ind-Vs-USA.jpg" }
           ],
           "2026-01-25": [
             { startHour: 19, startMinute: 0, endHour: 23, endMinute: 0, message: "3rd T20i", image: "https://i.ibb.co/QvBDYrvb/IMG-20260121-033723-733.jpg" }
